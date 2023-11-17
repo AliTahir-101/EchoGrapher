e05a242cd78e4e5bdc7409a89d51e64dee71fdb5
Ali Tahir <alitahir231@gmail.com>
2023-11-17 06:57:27 +0200

    Update to initialize portAudio lib

--- AudioProcessor.cpp
+++ AudioProcessor.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 int main()
 {
+    PaError err = Pa_Initialize(); // Calling Library initialization function
+    if (err != paNoError)
+    {
+        cerr << "PortAudio error: " << Pa_GetErrorText(err) << endl;
+        return 1;
+    }
+    else
+    {
+        cout << "PortAudio Api Initialized Successfully!" << endl;
+        Pa_Terminate();
+    }
+
     return 0;
 }
