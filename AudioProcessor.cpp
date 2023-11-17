f4866be3290474501e55e69d3459393c2b4118ea
Ali Tahir <alitahir231@gmail.com>
2023-11-17 07:35:26 +0200

    Update to Open an Audio Stream for Recording

--- AudioProcessor.cpp
+++ AudioProcessor.cpp
@@ -14,6 +14,37 @@ int main()
     else
     {
         cout << "PortAudio Api Initialized Successfully!" << endl;
+
+        PaStream *stream;
+        PaStreamParameters inputParameters;
+
+        inputParameters.device = Pa_GetDefaultInputDevice();
+        if (inputParameters.device == paNoDevice)
+        {
+            std::cerr << "No default input device." << std::endl;
+            return 1;
+        }
+
+        inputParameters.channelCount = 2;         // Stereo input
+        inputParameters.sampleFormat = paFloat32; // 32-bit floating point input
+        inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultLowInputLatency;
+        inputParameters.hostApiSpecificStreamInfo = NULL;
+
+        err = Pa_OpenStream(
+            &stream,
+            &inputParameters,
+            NULL,      // No output parameters for recording only
+            44100,     // Sample rate
+            256,       // Frames per buffer
+            paClipOff, // We won't output out-of-range samples so don't bother clipping them
+            NULL,      // No callback, use blocking API
+            NULL);     // No data for the callback since we're not using one
+        if (err != paNoError)
+        {
+            std::cerr << "PortAudio error: open stream: " << Pa_GetErrorText(err) << std::endl;
+            return 1;
+        }
+
         Pa_Terminate();
     }
 
