e437b2ce4862b3176196aa6b7a4f0b65a3d71dee
Ali Tahir <alitahir231@gmail.com>
2023-11-20 16:17:59 +0200

    Update to add the latest prog. on mainwindow header file.

--- EchoGrapherQT/mainwindow.h
+++ EchoGrapherQT/mainwindow.h
@@ -2,9 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "audioprocessor.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+    class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -15,7 +19,22 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    // Slot for handling the creation of the spectrogram visualization
+    void onNewSpectrogram(const QVector<float> &spectrum);
+    // Slot to handle errors from the audio processor
+    void onErrorOccurred(const QString &errorMessage);
+    // Slots to handle button clicks for starting and stopping processing
+    void startProcessing();
+    void stopProcessing();
+    void on_startButton_clicked();
+    void on_stopButton_clicked();
+
+    void on_selectOutputPathButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    AudioProcessor *audioProcessor; // Pointer to the AudioProcessor class
 };
+
 #endif // MAINWINDOW_H
